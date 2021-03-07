bfb382c49b75ed6f2d27d7753088c3143a2742ac
Josh Carter <joshcarter8400@gmail.com>
2021-03-07 17:16:16 -0500

    setup all routes, setup babel, tested routes

---
+++ mevn-stack/prod-server/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _routes = require('./routes');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+var port = 3000;
+
+(0, _routes.registerRoutes)(app);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, function () {
+  console.log('MEVN app listening at http://localhost:' + port);
+});
